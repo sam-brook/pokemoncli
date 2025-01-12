eed47fe54428c4794e3a28d459ba7232cf95c71d
Sam-Brook <brooksyhap@gmail.com>
2025-01-12 13:33:41 +0800

    idk lol

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(string) error
 }
 
 var commands map[string]cliCommand
@@ -29,13 +29,13 @@ func cleanInput(text string) string {
 	return result[0]
 }
 
-func commandExit() error {
+func commandExit(argument string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(argument string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Printf("Usage:\n\n")
 	for _, command := range commands {
@@ -54,7 +54,7 @@ type location_area struct {
 
 // NOTE for future self, if possible, make the requests and store them in a slice, then when going to a new location make more requests, but if the
 // current slice size is greater than the location requested, just loop through the slice and print the values there
-func commandMap() error {
+func commandMap(argument string) error {
 	for i := 0; i < 20; i++ {
 		url := "https://pokeapi.co/api/v2/location-area/" + strconv.Itoa(i+current_location_id)
 		val, exists := cache.Get(url)
@@ -97,7 +97,7 @@ func commandMap() error {
 	return nil
 }
 
-func commandMapB() error {
+func commandMapB(argument string) error {
 	if current_location_id > 20 {
 		current_location_id -= 20
 	}
@@ -146,6 +146,59 @@ func commandMapB() error {
 	return nil
 }
 
+type Response struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
+
+func commandExplore(argument string) error {
+	url := "https://pokeapi.co/api/v2/location-area/" + argument
+
+	val, exists := cache.Get(url)
+	if exists {
+		var location_response Response
+		err := json.Unmarshal(val, &location_response)
+		if err != nil {
+			return err
+		}
+		for _, encounter := range location_response.PokemonEncounters {
+			name := encounter.Pokemon.Name
+			fmt.Println(name)
+		}
+	} else {
+		res, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode > 299 {
+			failedErr := fmt.Sprintf("Response failed with status code %d", res.StatusCode)
+			return errors.New(failedErr)
+		}
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		var location_response Response
+		err = json.Unmarshal(val, &location_response)
+		if err != nil {
+			return err
+		}
+		for _, encounter := range location_response.PokemonEncounters {
+			name := encounter.Pokemon.Name
+			fmt.Println(name)
+		}
+		cache.Add(url, body)
+	}
+	return nil
+}
+
 func main() {
 	cache = pokecache.NewCache(2 * time.Second)
 
@@ -172,15 +225,21 @@ func main() {
 			description: "Displays the previous locations",
 			callback:    commandMapB,
 		},
+		"explore": {
+			name:        "explore",
+			description: "Displays the pokemon in a given location",
+			callback:    commandExplore,
+		},
 	}
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Pokedex > ")
 	for scanner.Scan() {
 		text := scanner.Text()
 		cleanText := cleanInput(text)
+		argument := ""
 		command, ok := commands[cleanText]
 		if ok {
-			err := command.callback()
+			err := command.callback(argument)
 			if err != nil {
 				fmt.Println(err)
 			}
